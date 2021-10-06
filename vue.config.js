48d33af7c891585576756b948ed97b2e6660bd83
thkim4729 <71238632+thkim4729@users.noreply.github.com>
2021-10-06 17:36:02 +0900

    config 수정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/kakao_busuiness_vue/' : '/',
     outputDir: 'docs',
     transpileDependencies: ['vuetify'],
 };
