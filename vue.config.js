67c8bc25a6bd761453d6d121b459066dd18dbaa3
Taehoon KIM <thkim4729@naver.com>
2021-10-23 14:31:11 +0900

    config.js 경로 수정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/kakao_busuiness_vue/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/kakao_business_vue/' : '/',
     outputDir: 'docs',
     transpileDependencies: ['vuetify'],
 };
